36df6909141bc7df57d6064be8fa63dafd4d79ff
ubuntu <ubuntu@ip-10-0-0-64>
2025-03-28 12:57:34 +0000

    waleko__o1_17dc__id_22

--- tests/test_websockets.py
+++ tests/test_websockets.py
@@ -112,6 +112,7 @@ async def test_ws_frame_get_not_completed_start():
 @pytest.mark.asyncio
 async def test_ws_frame_get_paused():
     assembler = WebsocketFrameAssembler(Mock())
+    assembler.protocol = Mock()
     assembler.message_complete = AsyncMock(spec=Event)
     assembler.message_complete.is_set = Mock(side_effect=[False, True])
     assembler.paused = True
